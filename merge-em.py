f16ba60a08a4dddcc36a2d1c777f4206fd57debb
BobH <bobh0303@users.noreply.github.com>
2026-02-14 18:29:45 -0700

    a little tidying up but mostly just comments

--- merge-em.py
+++ merge-em.py
@@ -12,7 +12,8 @@ inputCSVs = [
 
 outputCSV = 'master.csv'
 
-# compsite of all 
+# composite of all files processed is maintained in a dictionary
+# Key is the Google ID; value is the full dictionary row based on csv.DictReader rows
 master = dict()
 
 # Added column for tracing history:
@@ -25,7 +26,8 @@ for user, inputCSV in inputCSVs:
         outputFieldNames = list(reader.fieldnames)
         # Add .view fields
         for n in range(1,24):
-             outputFieldNames.insert(outputFieldNames.index(f'permissions.{n}.type')+1, f'permissions.{n}.view')
+             if (p := f'permissions.{n}.view') not in outputFieldNames:
+                outputFieldNames.insert(outputFieldNames.index(f'permissions.{n}.type')+1, p)
         # Add history field at the left edge
         outputFieldNames.insert(0, colHistory)
             
@@ -40,17 +42,18 @@ for user, inputCSV in inputCSVs:
             # merge into resuting "master" list:
             uid = row['File/Folder id']
             try:
+                # find the record for this uid in master
                 m_row = master[uid]
                 # resolve differences:
                 if row['Name'] != m_row['Name']:
                     # File renamed
                     m_row['Name'] = row['Name']
-                    m_row[colHistory] = f'{m_row[colHistory]}; {user}: rename'.strip('; ')
+                    m_row[colHistory] = f'{m_row[colHistory]}; {user}: rename'.removeprefix('; ')
                 p = int('0'+row['permissions'])
                 m_p = int('0'+m_row['permissions'])
                 if p > m_p:
-                    # Expanded permissions:
-                    row[colHistory] = f'{m_row[colHistory]}; {user}: perm {m_p}->{p}'.strip('; ')
+                    # Expanded permissions: replace full record (except for History)
+                    row[colHistory] = f'{m_row[colHistory]}; {user}: perm {m_p}->{p}'.removeprefix('; ')
                     master[uid] = row
             except KeyError:
                 # Not in master list -- add it
@@ -58,9 +61,11 @@ for user, inputCSV in inputCSVs:
             
 print(f'Found {len(master)} records')
 
+# use Byte-order-Mark encoding so Google and Excel know to process as UTF8
 with open(outputCSV, 'w', newline='', encoding='utf-8-sig') as outputCSVfile:
     writer = csv.DictWriter(outputCSVfile, fieldnames=outputFieldNames)
     writer.writeheader()
+    # output the master dictionary values sorted by path:
     for uid in sorted(master.keys(), key = lambda id: master[id]['Path']):
         writer.writerow(master[uid])
 
